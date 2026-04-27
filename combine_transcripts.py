ca62b98b2fcc886009f24d634918209b18429996
Ethan-Reyes <25EthanReyes@gmail.com>
2026-04-26 21:26:58 -0500

    Update combine_transcripts.py
    updated the path in combine transcripts.py

--- combine_transcripts.py
+++ combine_transcripts.py
@@ -1,7 +1,7 @@
 #this combines all the transcipts into one big file
 import os
 
-folder = r"C:\Users\Administrator\OneDrive\Documents\personal coding files\HTML-JS-CSS Projects\Website Project\Bill Scott Interview Recordings"
+folder = #change this to your path of your audio recordings
 
 combined_output = os.path.join(folder, "FULL_TRANSCRIPT.txt")
 
